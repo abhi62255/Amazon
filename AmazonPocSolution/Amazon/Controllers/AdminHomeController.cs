13e375e3df07dbdfdc82598a0f3bc178c09581a8
Abhishek Sharma <asharma1@xpanxion.com>
2019-08-09 18:38:00 +0530

    Just started 'Database'
    Signed-off-by: Abhishek Sharma <asharma1@xpanxion.com>

---
+++ AmazonPocSolution/Amazon/Controllers/AdminHomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Amazon.Controllers
+{
+    [Authorize(Users = "admin_123456")]
+    public class AdminHomeController : Controller
+    {
+        XKARTEntities _db = new XKARTEntities();
+        // GET: AdminHome
+        public ActionResult Index()
+        {
+            ViewBag.countrydrop = _db.CITies.Select(x => new SelectListItem { Text = x.City1, Value = x.CityId.ToString() }).ToList();
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult AddAddress()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddAddress(ADDRESS model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _db.ADDRESSes.Add(model);
+                _db.SaveChanges();
+                Session["ID"] = Convert.ToInt32( model.AddressId);
+                return RedirectToAction("AddSeller", "AdminHome");
+            }
+            return View(model);
+        }
+
+
+        [HttpGet]
+        public ActionResult AddSeller()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddSeller(SELLER model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                model.SellerAddressId = Convert.ToInt32( Session["ID"]);
+                _db.SELLERs.Add(model);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "AdminHome");
+            }
+            
+            return View(model);
+        }
+
+
+    }
+}
---
+++ AmazonPocSolution/Amazon/Controllers/RegisterLoginController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Amazon.Controllers
+{
+    public class RegisterLoginController : Controller
+    {
+        XKARTEntities _db = new XKARTEntities();
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Register(CUSTOMER model)
+        {
+                if (ModelState.IsValid)
+                {
+                    model.Password = Hashing.Hash(model.Password);
+                    model.ConfirmPassowrd = Hashing.Hash(model.ConfirmPassowrd);
+
+                    _db.CUSTOMERs.Add(model);
+                    _db.SaveChanges();
+                    FormsAuthentication.SetAuthCookie(model.Name, false);
+                    return RedirectToAction("Index", "Home");
+                }
+            return View(model);
+
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(CUSTOMER model)
+        {
+            model.Password = Hashing.Hash(model.Password);
+            int index = model.Email.IndexOf('@');
+            if (index == -1)
+            {
+                var user = _db.ADMINs.Where(a => a.Username.Equals(model.Email) && a.Password.Equals(model.Password)).FirstOrDefault();
+                if (user != null)
+                {
+                    FormsAuthentication.SetAuthCookie(user.Username, false);
+                    return RedirectToAction("Index", "AdminHome");
+                }
+                else
+                {
+                    ViewBag.message = "Wrong Credentials";
+                    return View();
+                }
+            }
+            else
+            {
+                var user = _db.CUSTOMERs.Where(a => a.Email.Equals(model.Email) && a.Password.Equals(model.Password)).FirstOrDefault();
+
+                if (user != null)
+                {
+                    FormsAuthentication.SetAuthCookie(user.Name, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ViewBag.message = "Wrong User Credentials";
+                    return View();
+                }
+            }
+        }
+
+        [HttpGet]
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
